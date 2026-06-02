30058e344c5bdbb869f3e5570c02efcca84f3dff
ZnaikaGeek <znaikageek@gmail.com>
2026-06-02 13:01:09 +0700

    Define Bookworm and Book types
    Added Bookworm and Book types to represent a book collection.

--- bookworm.go
+++ bookworm.go
@@ -1,1 +1,13 @@
+package main
 
+// Bookworm contains the list of books on a bookworm's shelf.
+type Bookworm struct {
+    Name  string `json:"name"`
+    Books []Book `json:"books"`
+}
+
+// Book describes a book on a bookworm's shelf.
+type Book struct {
+    Author string `json:"author"`
+    Title  string `json:"title"`
+}
