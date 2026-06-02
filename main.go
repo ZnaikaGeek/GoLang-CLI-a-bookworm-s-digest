2f201508a1b59964bd421e3c844051ce9704411d
ZnaikaGeek <znaikageek@gmail.com>
2026-06-02 13:53:47 +0700

    Enhance main function with error handling and comments
    Added error handling and comments for clarity.

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+    "fmt"
+    "os"
+)
+
 func main() {
-    // place to call logic
+/* Call the function with the path
+to the sample data
+*/
+    bookworms, err := loadBookworms("testdata/bookworms.json")
+
+    if err != nil {
+        /* If there's an ERROR,print it
+        to standart Error and exit
+        */
+        fmt.Fprintf(os.Stderr, "failed to load bookworms: %s\n", err)
+
+        os.Exit(1)
+    }
+/* Print the raw data to verify it was
+loaded correctly
+*/
+    fmt.Println(bookworms)
 }
